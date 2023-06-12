35ad20e40ca387b6d0dcbbc3fe49d0e2eb2ae9ba
biangomes <beanascigom@gmail.com>
2023-06-12 20:28:00 -0300

    Implementação de TreeSet em que o conjunto foi ordenado pelo alfabeto

--- src/secao19/application/Aula243.java
+++ src/secao19/application/Aula243.java
@@ -1,16 +1,18 @@
 package secao19.application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Aula243 {
     public static void main(String[] args) {
 
-        Set<String> set = new HashSet<>();
+        Set<String> set = new TreeSet<>();
 
         set.add("TV");
         set.add("Tablet");
         set.add("iPhone 13");
+        set.add("Xbox Series S|X");
+        set.add("Playstation 5");
 
         System.out.println(set.contains("iPhone 13"));
 
