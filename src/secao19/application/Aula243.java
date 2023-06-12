f3043a3da3c2bf0d7b79f28516a1a856cf4cf3c5
biangomes <beanascigom@gmail.com>
2023-06-12 20:28:44 -0300

    Implementacao do LinkedHashSet em que manteve a ordem que os elementos foram inseridos no conjunto

--- src/secao19/application/Aula243.java
+++ src/secao19/application/Aula243.java
@@ -1,18 +1,20 @@
 package secao19.application;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Aula243 {
     public static void main(String[] args) {
 
-        Set<String> set = new TreeSet<>();
+        Set<String> set = new LinkedHashSet<>();
 
         set.add("TV");
         set.add("Tablet");
         set.add("iPhone 13");
         set.add("Xbox Series S|X");
         set.add("Playstation 5");
+        set.add("Alexa");
 
         System.out.println(set.contains("iPhone 13"));
 
